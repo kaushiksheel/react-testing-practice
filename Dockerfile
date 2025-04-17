a46792bdeb352dced77a746cb63ae37d136bad49
kaushiksheel <kaushiksheel9@gmail.com>
2025-04-17 10:18:14 +0530

    feat : add docker-compose

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:alpine3.21
+
+WORKDIR /app
+
+COPY package.json package-lock.json* ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 5173
+
+CMD ["npm", "run", "dev"]
+
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true, // crucial for hot reload in Docker bind mounts
+    },
+  },
 });
